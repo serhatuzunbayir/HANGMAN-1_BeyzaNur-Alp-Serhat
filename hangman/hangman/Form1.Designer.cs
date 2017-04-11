66f39f48e88e60c932ef799d8e7ad4ab27ea44d9
Alp Erten <alperten95@gmail.com>
2017-04-11 15:17:09 +0300

    PROJECT FİRST VERSİON

---
+++ hangman/hangman/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace hangman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_kelime = new System.Windows.Forms.Label();
+            this.pcb_adam = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl_soylenenler = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbl_kategori = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_adam)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbl_kelime
+            // 
+            this.lbl_kelime.AutoSize = true;
+            this.lbl_kelime.Location = new System.Drawing.Point(66, 26);
+            this.lbl_kelime.Name = "lbl_kelime";
+            this.lbl_kelime.Size = new System.Drawing.Size(0, 13);
+            this.lbl_kelime.TabIndex = 0;
+            // 
+            // pcb_adam
+            // 
+            this.pcb_adam.BackColor = System.Drawing.Color.White;
+            this.pcb_adam.Location = new System.Drawing.Point(485, 16);
+            this.pcb_adam.Name = "pcb_adam";
+            this.pcb_adam.Size = new System.Drawing.Size(213, 472);
+            this.pcb_adam.TabIndex = 1;
+            this.pcb_adam.TabStop = false;
+            this.pcb_adam.Visible = false;
+            this.pcb_adam.Paint += new System.Windows.Forms.PaintEventHandler(this.pcb_adam_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbl_soylenenler);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.lbl_kategori);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.pcb_adam);
+            this.panel1.Controls.Add(this.lbl_kelime);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(780, 552);
+            this.panel1.TabIndex = 2;
+            this.panel1.SizeChanged += new System.EventHandler(this.panel1_SizeChanged);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // lbl_soylenenler
+            // 
+            this.lbl_soylenenler.AutoSize = true;
+            this.lbl_soylenenler.Location = new System.Drawing.Point(81, 304);
+            this.lbl_soylenenler.Name = "lbl_soylenenler";
+            this.lbl_soylenenler.Size = new System.Drawing.Size(0, 13);
+            this.lbl_soylenenler.TabIndex = 6;
+            this.lbl_soylenenler.Visible = false;
+            this.lbl_soylenenler.Click += new System.EventHandler(this.lbl_soylenenler_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 258);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Seç";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 261);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(53, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Visible = false;
+            // 
+            // lbl_kategori
+            // 
+            this.lbl_kategori.AutoSize = true;
+            this.lbl_kategori.Location = new System.Drawing.Point(171, 26);
+            this.lbl_kategori.Name = "lbl_kategori";
+            this.lbl_kategori.Size = new System.Drawing.Size(46, 13);
+            this.lbl_kategori.TabIndex = 3;
+            this.lbl_kategori.Text = "Kategori";
+            this.lbl_kategori.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(78, 369);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(333, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Oyuna Başla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button2;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 576);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_adam)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_kelime;
+        private System.Windows.Forms.PictureBox pcb_adam;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbl_kategori;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbl_soylenenler;
+    }
+}
+
---
+++ hangman/hangman/Form1.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace hangman
+{
+
+    //we define a class in here.//********************************CLASS******************************************//
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] harfler = { "a", "b", "c", "ç", "d", "e", "f", "g", "ğ", "h", "ı", "i", "j", "k", "l", "m", "n", "o", "ö", "p", "r", "s", "ş", "t", "u", "ü", "v", "y", "z", "x", "q", "w" };
+        Adam_Asmaca_islemleri cl_adam_asmaca;
+        string kelime;
+        string[] secilen_harfler;
+        int can_hakki = 7;
+        int kalan_hakkimiz;
+        int width, height;
+
+        //**************************************HANGMAN**************************************//
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            konum_ayarla();
+            width = pcb_adam.Width;
+            height = pcb_adam.Height;
+            kalan_hakkimiz = can_hakki;
+            secilen_harfler = new string[0];
+            cl_adam_asmaca = new Adam_Asmaca_islemleri();
+        }
+
+        //**************************************HANGMAN**************************************//
+        private void kelimeyi_Sec()
+        {
+           Adam_Asmaca_islemleri.Yeni_Kelime kelimemiz_ = cl_adam_asmaca.kelime_al();
+            lbl_kategori.Text = "Bu bir " + kelimemiz_.kategori;
+            kelime = kelimemiz_.kelime;
+            foreach (char harf in kelime.ToCharArray())
+            {
+                lbl_kelime.Text = lbl_kelime.Text + "_ ";
+            }
+            button1.Visible = false;
+        }
+
+        //**************************************HANGMAN**************************************//
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pcb_adam.Visible = true;
+            textBox1.Visible = true;
+            lbl_soylenenler.Visible = true;
+            lbl_kelime.Visible = true;
+            lbl_kategori.Visible = true;
+            button2.Visible = true;
+            kelimeyi_Sec();
+        }
+
+        //**************************************HANGMAN**************************************//
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string veri = textBox1.Text;
+            bool buldu = false;
+            bool soylendi = false;
+            foreach(var item in secilen_harfler)
+            {
+                if(veri.ToLower() == item.ToLower())
+                {
+                    soylendi = true;
+                    MessageBox.Show("Bu harfi daha önce söylediniz");
+                }
+            }
+            if (!soylendi)
+            {
+                for (int i = 0; i < harfler.Length; i++)
+                {
+                    if (veri.ToLower() == harfler[i])
+                    {
+                        buldu = true;
+                        kelimede_bul(veri);
+                        break;
+                    }
+                }
+                if (!buldu)
+                {
+                    MessageBox.Show("Geçerli bir harf yazınız");
+                }
+                guncelle();
+            }
+            int index = lbl_kelime.Text.IndexOf("_");
+            if(index == -1)
+            {
+                sonraki_bolum();
+            }
+        }
+
+        //**************************************HANGMAN**************************************//
+        private void kelimede_bul(string harf)
+        {
+            bool dogru_mu = false;
+            char[] dizi = kelime.ToCharArray();
+            for (int a=0; a < dizi.Length; a++)
+            {
+                if(dizi[a].ToString() != "")
+                {
+                    if (harf == dizi[a].ToString().ToLower())
+                    {
+                        dogru_mu = true;
+                        lbl_kelime.Text = lbl_kelime.Text.Remove(a * 2, 1);
+                        lbl_kelime.Text = lbl_kelime.Text.Insert(a * 2, harf).ToUpper();
+
+                    }
+                }
+            }
+            if (!dogru_mu)
+            {
+                
+                pcb_adam.Invalidate();
+                kalan_hakkimiz--;
+                
+            }
+            ekle_harf(harf);
+        }
+
+        //**************************************HANGMAN**************************************//
+        private void guncelle()
+        {
+            lbl_soylenenler.Text = "";
+            foreach(string item in secilen_harfler)
+            {
+                lbl_soylenenler.Text = lbl_soylenenler.Text + item + " ";
+            }
+        }
+
+        //**************************************HANGMAN**************************************//
+        private void ekle_harf(string harf)
+        {
+            string[] a = new string[secilen_harfler.Length + 1];
+            for(int i= 0; i < secilen_harfler.Length; i++)
+            {
+                a[i] = secilen_harfler[i];
+            }
+            a[a.Length - 1] = harf;
+            secilen_harfler = a;
+        }
+
+        //**************************************HANGMAN**************************************//
+        private void pcb_adam_Paint(object sender, PaintEventArgs e)
+        {
+            Pen kalem = new Pen(Color.Black, 12);
+            if (kalan_hakkimiz != -1)
+            {
+                if (kalan_hakkimiz < can_hakki)
+                {
+                    e.Graphics.DrawLine(kalem, width / 10, height / 15, width / 10, height / 15 * 14);
+                    e.Graphics.DrawLine(kalem, width / 10, height / 15, width / 2, height / 15);
+                }
+                if (kalan_hakkimiz < can_hakki - 1)
+                {
+                    e.Graphics.DrawLine(kalem, width / 2, height / 15, width / 2, height / 15 * 3);
+                    e.Graphics.DrawEllipse(kalem, width / 2 - width / 20, height / 5, width / 10, height / 10);
+                }
+                if (kalan_hakkimiz < can_hakki - 2)
+                {
+                    e.Graphics.DrawLine(kalem, width / 2, height / 10 * 3, width / 2, height / 10 * 6);
+                }
+                if (kalan_hakkimiz < can_hakki - 3)
+                {
+                    e.Graphics.DrawLine(kalem, width / 2, height / 10 * 3, width / 2 + width / 10, height / 10 * 3 + height / 5);
+                }
+                if (kalan_hakkimiz < can_hakki - 4)
+                {
+                    e.Graphics.DrawLine(kalem, width / 2, height / 10 * 3, width / 2 - width / 10, height / 10 * 3 + height / 5);
+                }
+                if (kalan_hakkimiz < can_hakki - 5)
+                {
+                    e.Graphics.DrawLine(kalem, width / 2, height / 10 * 6, width / 2 + width / 10, height / 10 * 6 + height / 10);
+                }
+                if (kalan_hakkimiz < can_hakki - 6)
+                {
+                    e.Graphics.DrawLine(kalem, width / 2, height / 10 * 6, width / 2 - width / 10, height / 10 * 6 + height / 10);
+
+                }
+            }else { oyun_bitti(); }
+     
+
+      }
+
+        //**************************************HANGMAN**************************************//
+        private void sonraki_bolum()
+        {
+            temizle();
+            kelimeyi_Sec();
+        }
+
+        //**************************************HANGMAN**************************************//
+        private void oyun_bitti()
+        {
+            MessageBox.Show("Oyun Bitti. Doğru kelime: "+kelime);
+            button1.Visible = true;
+            textBox1.Visible = false;
+            button2.Visible = false;
+            temizle();
+            pcb_adam.Visible = false;
+        }
+
+        //**************************************HANGMAN**************************************//
+        private void temizle()
+        {
+            secilen_harfler = new string[0];
+            kalan_hakkimiz = can_hakki;
+            pcb_adam.Invalidate();
+            lbl_kategori.Text = "";
+            lbl_kelime.Text = "";
+            lbl_soylenenler.Text = "";
+            textBox1.Text = "";
+        }
+
+        //**************************************HANGMAN**************************************//
+        private void lbl_soylenenler_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //**************************************HANGMAN**************************************//
+
+        private void panel1_SizeChanged(object sender, EventArgs e)
+        {
+            konum_ayarla();
+        }
+
+        //**************************************HANGMAN**************************************//
+        public void konum_ayarla()
+        {
+            int width, height;
+            width = this.Width;
+            height = this.Height;
+            panel1.Width = width;
+            panel1.Height = height;
+            panel1.Location = new Point(0, 0);
+            pcb_adam.Width = width / 9 * 4;
+            pcb_adam.Height = height / 4 * 3;
+            pcb_adam.Location = new Point(width - pcb_adam.Width - 10,10);
+            lbl_kategori.Location = new Point(10, 10);
+            lbl_kelime.Location = new Point(10, 50);
+            lbl_soylenenler.Location = new Point(10, 120);
+            button1.Size = new System.Drawing.Size(width / 3, height / 10);
+            textBox1.Location = new Point(10,90);
+            button1.Location = new Point(width / 2-button1.Width/2, height / 2-button1.Height/2);
+            button2.Location = new Point(70,90);
+            lbl_soylenenler.Location = new Point(10, height / 2);
+        }
+
+        //**************************************HANGMAN**************************************//
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+    //we define a class in here.//********************************CLASS******************************************//
+    class Adam_Asmaca_islemleri
+    {
+        private List<Yeni_Kelime> yeni_kelime_listesi;
+        private List<Yeni_Kelime> kelime_liste;
+        public Adam_Asmaca_islemleri()
+        {
+            data_dosyasini_al();
+        }
+       public struct Yeni_Kelime
+        {
+            public string kelime;
+            public string kategori;
+        }
+        public Yeni_Kelime kelime_al()
+        {
+            if (kelime_liste.Count == 0)
+            {
+                kelime_liste = yeni_kelime_listesi;
+            }
+            Random rastg = new Random();
+            int a = rastg.Next(0, kelime_liste.Count);
+            Yeni_Kelime kelimemiz = kelime_liste[a];
+            return kelimemiz;
+        }
+        public Yeni_Kelime Yeni_kelime_olustur(string kelime,string kategori)
+        {
+            Yeni_Kelime kelimemiz = new Yeni_Kelime();
+            kelimemiz.kelime = kelime;
+            kelimemiz.kategori = kategori;
+                return kelimemiz;
+        }
+        private void data_dosyasini_al()
+        {
+            yeni_kelime_listesi = new List<Yeni_Kelime>();
+            StreamReader oku = new StreamReader(@"data.txt",Encoding.Default);
+            string veri;
+            while((veri=oku.ReadLine()) != null)
+            {
+                if(veri != "")
+                {
+                    yeni_kelime_listesi.Add(Yeni_kelime_olustur(veri.Split(',')[0], veri.Split(',')[1]));
+                }
+            }
+            kelime_liste = yeni_kelime_listesi;
+        }
+    }
+}
